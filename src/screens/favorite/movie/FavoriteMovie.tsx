66add5452e8a904852239854b2ff4ad8005e2051
Mehedi Hassan Piash <piash599@gmail.com>
2024-11-07 23:58:46 +0600

    - Implemented ConfirmationAlert.tsx for favorite movie and tv series

--- src/screens/favorite/movie/FavoriteMovie.tsx
+++ src/screens/favorite/movie/FavoriteMovie.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FlatList, View } from 'react-native';
 import styles from './FavoriteMovie.style.ts';
 import { useFavoriteStore } from '../../../store/FavoriteStore.ts';
@@ -6,34 +6,63 @@ import FavoriteComponent from '../../../components/favorite/FavoriteComponent.ts
 import { MovieDetail } from '../../../types/MovieDetail.ts';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { RootStackParam } from '../../../types/navigation/NavigationTypes.ts';
+import ConfirmationAlert from '../../../components/alert-dialog/ConfirmationAlert.tsx';
 
 type FavoriteMovieNavigationProp = NavigationProp<
   RootStackParam,
   'MovieDetail'
 >;
+
 const FavoriteMovie = () => {
   const navigation = useNavigation<FavoriteMovieNavigationProp>();
   const { favoriteMovies, toggleFavoriteMovie } = useFavoriteStore();
-  const favorite = ({ item }: { item: MovieDetail }) => {
-    return (
-      <FavoriteComponent
-        title={item.title}
-        poster_path={item.poster_path}
-        onPress={() => navigation.navigate('MovieDetail', { movieId: item.id })}
-        onRemove={() => {
-          toggleFavoriteMovie(item);
-        }}
-      />
-    );
+
+  const [visible, setVisible] = useState(false);
+  const [movieToRemove, setMovieToRemove] = useState<MovieDetail | null>(null);
+
+  const showDialog = (movie: MovieDetail) => {
+    setMovieToRemove(movie);
+    setVisible(true);
   };
+
+  const hideDialog = () => {
+    setVisible(false);
+    setMovieToRemove(null);
+  };
+
+  const confirmRemoveFavorite = () => {
+    if (movieToRemove) {
+      toggleFavoriteMovie(movieToRemove);
+    }
+    hideDialog();
+  };
+
+  const favorite = ({ item }: { item: MovieDetail }) => (
+    <FavoriteComponent
+      title={item.title}
+      poster_path={item.poster_path}
+      onPress={() => navigation.navigate('MovieDetail', { movieId: item.id })}
+      onRemove={() => showDialog(item)}
+    />
+  );
+
   return (
     <View style={styles.mainView}>
       <FlatList
         style={styles.flatListStyle}
         data={favoriteMovies}
         renderItem={favorite}
+        keyExtractor={(item) => item.id.toString()} // Ensure a unique key for each item
+      />
+      <ConfirmationAlert
+        visible={visible}
+        title={'Remove Favorite'}
+        message={'Are you sure you want to remove from your favorites?'}
+        onConfirm={confirmRemoveFavorite}
+        onCancel={hideDialog}
       />
     </View>
   );
 };
+
 export default FavoriteMovie;
--- src/screens/favorite/tvseries/FavoriteTvSeries.tsx
+++ src/screens/favorite/tvseries/FavoriteTvSeries.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './FovoriteTvSeries.style.ts';
 import { FlatList, View } from 'react-native';
 import { useFavoriteStore } from '../../../store/FavoriteStore.ts';
@@ -6,6 +6,8 @@ import FavoriteComponent from '../../../components/favorite/FavoriteComponent.ts
 import { TvSeriesDetail } from '../../../types/TvSeriesDetail.ts';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { RootStackParam } from '../../../types/navigation/NavigationTypes.ts';
+import ConfirmationAlert from '../../../components/alert-dialog/ConfirmationAlert.tsx';
+import { MovieDetail } from '../../../types/MovieDetail.ts';
 
 type FavoriteTvSeriesNavigationProp = NavigationProp<
   RootStackParam,
@@ -15,6 +17,27 @@ type FavoriteTvSeriesNavigationProp = NavigationProp<
 const FavoriteTvSeries = () => {
   const navigation = useNavigation<FavoriteTvSeriesNavigationProp>();
   const { favoriteTvSeries, toggleFavoriteTvSeries } = useFavoriteStore();
+  const [visible, setVisible] = useState(false);
+  const [tvSeriesToRemove, setTvSeriesToRemove] =
+    useState<TvSeriesDetail | null>(null);
+
+  const showDialog = (movie: TvSeriesDetail) => {
+    setTvSeriesToRemove(movie);
+    setVisible(true);
+  };
+
+  const hideDialog = () => {
+    setVisible(false);
+    setTvSeriesToRemove(null);
+  };
+
+  const confirmRemoveFavorite = () => {
+    if (tvSeriesToRemove) {
+      toggleFavoriteTvSeries(tvSeriesToRemove);
+    }
+    hideDialog();
+  };
+
   const favorite = ({ item }: { item: TvSeriesDetail }) => {
     return (
       <FavoriteComponent
@@ -23,9 +46,7 @@ const FavoriteTvSeries = () => {
         onPress={() => {
           navigation.navigate('TvSeriesDetail', { tvSeriesId: item.id });
         }}
-        onRemove={() => {
-          toggleFavoriteTvSeries(item);
-        }}
+        onRemove={() => showDialog(item)}
       />
     );
   };
@@ -35,6 +56,13 @@ const FavoriteTvSeries = () => {
         style={styles.flatListStyle}
         data={favoriteTvSeries}
         renderItem={favorite}
+      />
+      <ConfirmationAlert
+        visible={visible}
+        title={'Remove Favorite'}
+        message={'Are you sure you want to remove from your favorites?'}
+        onConfirm={confirmRemoveFavorite}
+        onCancel={hideDialog}
       />
     </View>
   );
